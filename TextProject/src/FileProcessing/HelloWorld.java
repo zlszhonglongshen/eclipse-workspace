990bb839084320a8793490f09984e79250010867
zhonglongshen <593956670@qq.com>
2018-12-04 15:52:44 +0800

    update

--- TextProject/src/FileProcessing/HelloWorld.java
+++ TextProject/src/FileProcessing/HelloWorld.java
@@ -1,11 +1,9 @@
 package FileProcessing;
 
-
-
 /**
  * 
 * <p>Title: HelloWorld</p>
-* <p>Description: </p>
+* <p>Description:第一个程序 </p>
 * @author:johnson.zhong
 * @date 2018年12月4日下午2:31:52
  */
@@ -13,6 +11,7 @@ package FileProcessing;
 //第一个程序
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.print("Hellp world!!!");
+		System.out.print("Hellp world!!!"+"\n");
+		System.out.println("我是一个菜鸟!!!");
 	}
 }
